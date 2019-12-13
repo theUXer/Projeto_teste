432df198daca18c4d7c0617380ea6703ce2f8463
danilozp <danilo.lopes@zephyrplace.com>
2019-12-13 13:37:31 -0300

    card

--- src/Screens/Principal/index.js
+++ src/Screens/Principal/index.js
@@ -1,13 +1,136 @@
-import React from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import React, { useState, Component } from 'react'
 
 
-export default function Pincipal({ navigation }) {
+import { Card, CardItem, Thumbnail, Text, Icon, Left, Body, Input } from 'native-base'
+
+
+
+export default function Principal(props) {
     return (
-        <>
+        <Background color='white'>
+
+            <Search placeholder='Pesquise aqui...' />
             <ScrollView>
-                <Text>Só pra ver se foi.</Text>
+                <Card>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={} />
+                            <Body>
+                                <Text>Zuleika</Text>
+                                <Text note>Motorista</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                        <Image source={} />
+                    </CardItem>
+                    <CardItem>
+                        <Button>
+                            <Icon active name="thumbs-up" />
+                            <Comments>53 Likes</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="chatbubbles" />
+                            <Comments>Contact</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="heart" />
+                            <Comments>Favorite</Comments>
+                        </Button>
+                    </CardItem>
+                </Card>
+
+
+                <Card>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={} />
+                            <Body>
+                                <Text>Maria Luiza</Text>
+                                <Text note>Babá</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                        <Image source={} />
+                    </CardItem>
+                    <CardItem>
+                        <Button>
+                            <Icon active name="thumbs-up" />
+                            <Comments>53 Likes</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="chatbubbles" />
+                            <Comments>Contact</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="heart" />
+                            <Comments>Favorite</Comments>
+                        </Button>
+                    </CardItem>
+                </Card>
+
+
+                <Card>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={} />
+                            <Body>
+                                <Text>João Carlos</Text>
+                                <Text note>Eletricista</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                        <Image source={} />
+                    </CardItem>
+                    <CardItem>
+                        <Button>
+                            <Icon active name="thumbs-up" />
+                            <Comments>53 Likes</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="chatbubbles" />
+                            <Comments>Contact</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="heart" />
+                            <Comments>Favorite</Comments>
+                        </Button>
+                    </CardItem>
+                </Card>
+
+
+                <Card>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={} />
+                            <Body>
+                                <Text>Emerson Silveira</Text>
+                                <Text note>Empreiteiro</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                        <Image source={} />
+                    </CardItem>
+                    <CardItem>
+                        <Button>
+                            <Icon active name="thumbs-up" />
+                            <Comments>53 Likes</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="chatbubbles" />
+                            <Comments>Contact</Comments>
+                        </Button>
+                        <Button>
+                            <Icon active name="heart" />
+                            <Comments>Favorite</Comments>
+                        </Button>
+                    </CardItem>
+                </Card>
             </ScrollView>
-        </>
-    )
+        </ Background>
+
+    );
 }
